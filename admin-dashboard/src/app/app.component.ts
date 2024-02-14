ccc4f37fbe9d85723dbc3df83703e5f031fda0ee
basmaelkenawy <belkenawy@gmail.com>
2024-02-14 11:28:21 +0200

    design home ,user and post

--- admin-dashboard/src/app/app.component.ts
+++ admin-dashboard/src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { UsersComponent } from './components/users/users.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:[
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent,
+    PostsComponent,
+    UsersComponent,
+    SettingComponent,
+    LogoutComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'admin-dashboard';
+
+
 }
--- admin-dashboard/src/app/app.routes.ts
+++ admin-dashboard/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { UsersComponent } from './components/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"users",component:UsersComponent},
+  {path:"posts",component:PostsComponent},
+  {path:"setting",component:SettingComponent},
+  {path:"logout",component:LogoutComponent}
+];
