ad0c4d9ebb75e833660807e54809d83cdb71e779
ibthal_mohamed <ibthal923@gmail.com>
2024-02-20 17:31:46 +0200

    fetch user data

--- admin-dashboard/src/app/components/users/users.component.ts
+++ admin-dashboard/src/app/components/users/users.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UsersService } from '../../Services/users.service';
+
 import { HeaderComponent } from '../header/header.component';
+import { OneUserComponent } from '../one-user/one-user.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, OneUserComponent],
+  providers: [UsersService],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
-export class UsersComponent {
-
+export class UsersComponent implements OnInit {
+  constructor(private UService: UsersService) {}
+  Users: any;
+  ngOnInit() {
+    this.UService.GetAllUsers().subscribe({
+      next: (data) => {
+        //console.log(data)
+        this.Users = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
